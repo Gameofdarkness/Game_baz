0efbc7fcac50e8fcf11c76ba310a71625c978788
diyar-2020 <dyartymwry1@gmail.com>
2020-05-06 23:59:39 +0430

    new game

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1588793355,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"bg_logo.png",
+		"logo-sheet0.png",
+		"bg_menu.png",
+		"telegram_icon-sheet0.png",
+		"bg_game.png",
+		"block1-sheet0.png",
+		"letf_b-sheet0.png",
+		"right_b-sheet0.png",
+		"ward_slide-sheet0.png",
+		"sprite-sheet0.png",
+		"sprite2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
